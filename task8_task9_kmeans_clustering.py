43f087df12bdb19dfd71dd221b32a51b900aba71
mpkokaia <marykokaiya@gmail.com>
2014-05-04 11:14:30 +0600

    k- means fix

--- task8_task9_kmeans_clustering.py
+++ task8_task9_kmeans_clustering.py
@@ -4,55 +4,55 @@ import re
 import random
 
 
-def pearson(v1, v2):
-    sum1 = sum(v1)
-    sum2 = sum(v2)
-    sum1Sq = sum([pow(v, 2) for v in v1])
-    sum2Sq = sum([pow(v, 2) for v in v2])
-    pSum = sum([v1[i] * v2[i] for i in range(len(v1))])
-    num = pSum - (sum1 * sum2 / len(v1))
-    den = pow((sum1Sq - pow(sum1, 2) / len(v1)) * (sum2Sq - pow(sum2, 2) / len(v1)), 1 / 2)
-    if den == 0: return 0
-    return 1.0 - num / den
+class KCluster(object):
+    def __init__(self, data, k):
+        self.rotatematrix(data)
+        self.k = k
 
+    def rotatematrix(self, data):
+        self.data = []
+        for i in range(len(data[0])):
+            newrow = [data[j][i] for j in range(len(data))]
+            self.data.append(newrow)
 
-class bicluster:
-    def __init__(self, vec, left=None, right=None, distance=0.0, id=None):
-        self.left = left
-        self.right = right
-        self.vec = vec
-        self.id = id
-        self.distance = distance
+    def pearson(self, v1, v2):
+        sum1 = sum(v1)
+        sum2 = sum(v2)
+        sum1Sq = sum([pow(v, 2) for v in v1])
+        sum2Sq = sum([pow(v, 2) for v in v2])
+        pSum = sum([v1[i] * v2[i] for i in range(len(v1))])
+        num = pSum - (sum1 * sum2 / len(v1))
+        den = pow((sum1Sq - pow(sum1, 2) / len(v1)) * (sum2Sq - pow(sum2, 2) / len(v1)), 1 / 2)
+        if den == 0: return 0
+        return 1.0 - num / den
 
-
-def kcluster(rows, distance, k):
-    ranges = [(min([row[i] for row in rows]), max([row[i] for row in rows])) for i in range(len(rows[0]))]
-    clusters = [[random.random() * (ranges[i][1] - ranges[i][0]) + ranges[i][0] for i in range(len(rows[0]))] for j in
-                range(k)]
-    lastmatches = None
-    for t in range(100):
-        bestmatches = [[] for i in range(k)]
-        for j in range(len(rows)):
-            row = rows[j]
-            bestmatch = 0
-            for i in range(k):
-                d = distance(clusters[i], row)
-                if d < distance(clusters[bestmatch], row):
-                    bestmatch = i
-            bestmatches[bestmatch].append(j)
-        if bestmatches == lastmatches: break
-        lastmatches = bestmatches
-        for i in range(k):
-            avgs = [0.0] * len(rows[0])
-            if len(bestmatches[i]) > 0:
-                for rowid in bestmatches[i]:
-                    for m in range(len(rows[rowid])):
-                        avgs[m] += rows[rowid][m]
-                for j in range(len(avgs)):
-                    avgs[j] /= len(bestmatches[i])
-                clusters[i] = avgs
-    return bestmatches
-
+    def clustering(self):
+        ranges = [(min([row[i] for row in self.data]), max([row[i] for row in self.data])) for i in range(len(self.data[0]))]
+        clusters = [[random.random() * (ranges[i][1] - ranges[i][0]) + ranges[i][0] for i in range(len(self.data[0]))] for j in
+                    range(self.k)]
+        lastmatches = None
+        for t in range(100):
+            bestmatches = [[] for i in range(self.k)]
+            for j in range(len(self.data)):
+                row = self.data[j]
+                bestmatch = 0
+                for i in range(self.k):
+                    d = self.pearson(clusters[i], row)
+                    if d < self.pearson(clusters[bestmatch], row):
+                        bestmatch = i
+                bestmatches[bestmatch].append(j)
+            if bestmatches == lastmatches: break
+            lastmatches = bestmatches
+            for i in range(self.k):
+                avgs = [0.0] * len(self.data[0])
+                if len(bestmatches[i]) > 0:
+                    for rowid in bestmatches[i]:
+                        for m in range(len(self.data[rowid])):
+                            avgs[m] += self.data[rowid][m]
+                    for j in range(len(avgs)):
+                        avgs[j] /= len(bestmatches[i])
+                    clusters[i] = avgs
+        return bestmatches
 
 def read_data():
     rownames = []
@@ -79,7 +79,6 @@ def read_data():
                 if data == interes:
                     if st['vk_id'] not in tmp_rownames:
                         tmp_rownames.append(st['vk_id'])
-
     data = []
     for i in tmp_rownames:
         tmp = []
@@ -94,19 +93,14 @@ def read_data():
             rownames.append(i)
     return colnames, rownames, data
 
-
-def rotatematrix(data):
-    newdata = []
-    for i in range(len(data[0])):
-        newrow = [data[j][i] for j in range(len(data))]
-        newdata.append(newrow)
-    return newdata
-
-
 interests, people, data = read_data()
-data = rotatematrix(data)
-kclust = kcluster(data, distance=pearson, k=27)
+kclust = KCluster(data, 27).clustering()
+f = open('k_means_result.txt','w')
+counter = 1
 for i in range(27):
-    print('\n')
+    f.write(str(counter) + '\n')
+    counter += 1
     for r in kclust[i]:
-        print(interests[r])
+        f.write(interests[r])
+        f.write('\n')
+f.close()
